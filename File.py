27c6cb8e51dfee9255e393947c28f02cb7d19c1e
vadim <vadim.zaripov1994@gmail.com>
2017-10-25 20:36:10 +0200

    Testing some python stuff

---
+++ File.py
@@ -0,0 +1,24 @@
+#!/bin/bash/env python3
+from bs4 import BeautifulSoup
+import requests
+import re
+path = '/home/vadim/Desktop/results'
+linkList = []
+
+
+class SearchItems:
+    def __init__(self):
+        term = input('Enter your searchterm: ')
+        page = requests.get("http://www.google.de/search?q="+term)
+        soup = BeautifulSoup(page.content, "lxml")
+        links = soup.findAll("a")
+        for link in soup.find_all("a", href=re.compile("(?<=/url\?q=)(htt.*://.*)")):
+            linkList.append(re.split(":(?=http)", link["href"].replace("/url?q=","")))
+            with open(path, mode='wt', encoding='utf-8') as myfile:
+                for lines in linkList:
+                    myfile.write('\n'.join(str(line) for line in lines))
+                    myfile.write('\n')
+        print('File was written successfully')
+
+
+SearchItems()
